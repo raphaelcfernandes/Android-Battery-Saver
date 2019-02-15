6cd73798aed2d41fa8a54231743af58bd704ba06
bwatkins6 <baw76@pitt.edu>
2019-02-15 17:20:18 -0500

    second commit with updated notification

--- app/src/main/java/com/example/raphael/tcc/AppUI/SpeedUpNotification.java
+++ app/src/main/java/com/example/raphael/tcc/AppUI/SpeedUpNotification.java
@@ -1,16 +1,25 @@
 package com.example.raphael.tcc.AppUI;
 
 import android.support.v4.app.NotificationCompat;
+import android.content.Intent;
 
 import com.example.raphael.tcc.R;
 
+/*
+    Android Battery Saver
+    This class will handle the notification pop up to increase speed.
+
+ */
 public class SpeedUpNotification
 {
-    NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this, CHANNEL_ID)
+    Intent speedUpIntent = new Intent(this, AlertDetails.class);
+            speedUpIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+
+    NotificationCompat.Builder mBuilder = new NotificationCompat.Builder()
             .setSmallIcon(R.drawable.notification_icon)
             .setContentTitle("CPU Speed Manager")
             .setContentText("Tap the notification to increase speed.")
             .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-            .setContentIntent(pendingIntent)
-            .setAutoCancel(true);
+            .setContentIntent()
+            .setOngoing(true);
 }
