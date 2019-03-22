3e7da3ed5ea40913dc4fdd69c7465b03a3b374a5
bwatkins6 <baw76@pitt.edu>
2019-03-22 03:08:01 -0400

    begin adding support for usage stats

--- app/src/main/java/com/example/raphael/tcc/AppUI/ViewPagerFragments/UsageStats.java
+++ app/src/main/java/com/example/raphael/tcc/AppUI/ViewPagerFragments/UsageStats.java
@@ -5,11 +5,26 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import com.example.raphael.tcc.R;
 
+import java.util.ArrayList;
+
+/*
+    Android Battery Saver
+    baw76 Capstone Research Spring 2019
+    -This class handles the statistics for the algorithms and displays them to the user.
+    *****Still in progress
+ */
+
 public class UsageStats extends Fragment
 {
+    ListView leftList;
+    ListView rightList;
+    ArrayList leftArrayList;
+    ArrayList rightArrayList;
+
     public static UsageStats newInstance()
     {
         UsageStats newUS = new UsageStats();
@@ -28,6 +43,10 @@ public class UsageStats extends Fragment
     public View onCreateView(LayoutInflater inflate, ViewGroup container, Bundle savedInstanceState)
     {
         View newView = inflate.inflate(R.layout.usage_stats, container, false);
+
+        leftList = newView.findViewById(R.id.left_list);
+        rightList = newView.findViewById(R.id.right_list);
+
         return newView;
     }
 }
